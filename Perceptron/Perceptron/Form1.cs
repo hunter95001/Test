44b5c219215a0826c2c0cd72e60213e90bbefed3
hunter95001 <42367760+hunter95001@users.noreply.github.com>
2018-09-10 19:08:59 +0900

    퍼셉트론 입니다.
    잘못만들었어요.

---
+++ Perceptron/Perceptron/Form1.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Perceptron
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap Original_Bitmap;//원본
+        private Bitmap Rayleigh_Bitmap;
+        private Bitmap Ruffy_Bitmap;
+        private Bitmap Shanks_Bitmap;
+        private Bitmap Ace_Bitmap;
+        private Color color;
+
+        private int[][] input_bitmap = new int[4][];
+        public Form1(){
+            InitializeComponent();
+            Rayleigh_Bitmap = new Bitmap(Image.FromFile("실버즈레일리.jpg"));
+            Ruffy_Bitmap = new Bitmap(Image.FromFile("루피.jpg"));
+            Shanks_Bitmap = new Bitmap(Image.FromFile("샹크스.jpg"));
+            Ace_Bitmap = new Bitmap(Image.FromFile("에이스1.png"));
+        }
+        private int area(Bitmap setBitmap) {
+            return setBitmap.Width * setBitmap.Height;
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            Binary(Rayleigh_Bitmap);
+            Binary(Ruffy_Bitmap);
+            Binary(Shanks_Bitmap);
+            Binary(Ace_Bitmap);
+            int val = area(Rayleigh_Bitmap);
+            input_bitmap[0] = new int[val];
+            val = area(Ruffy_Bitmap);
+            input_bitmap[1] = new int[val];
+            val = area(Shanks_Bitmap);
+            input_bitmap[2] = new int[val];
+            val = area(Ace_Bitmap);
+            input_bitmap[3] = new int[val];
+          //  Console.WriteLine(val);
+
+
+            int ss = 0;
+            for (int i = 0; i < input_bitmap.Length; i++)
+                    for (int x = 0; x < Rayleigh_Bitmap.Width; x++)
+                        for (int y = 0; y < Rayleigh_Bitmap.Height; y++){
+                            color = Rayleigh_Bitmap.GetPixel(x, y);
+                        input_bitmap[i][(x * Rayleigh_Bitmap.Width) + y] =color.R;
+                        }
+                
+            
+
+            Perceptron_init perceptron = new Perceptron_init(input_bitmap);
+            pictureBox1.Image = Rayleigh_Bitmap;
+            pictureBox2.Image = Ruffy_Bitmap;
+            pictureBox3.Image = Shanks_Bitmap;
+            pictureBox4.Image = Ace_Bitmap;
+        }
+        private void Binary(Bitmap Set_Bitmap) {
+            int max=0;
+            int bright;
+            for (int x = 0; x < Set_Bitmap.Width; x++)
+                for (int y = 0; y < Set_Bitmap.Height; y++) { 
+                    color = Set_Bitmap.GetPixel(x , y );
+                    max =max+color.R;
+                         }
+                max = max / (Set_Bitmap.Width * Set_Bitmap.Height);
+                for (int x = 0; x<Set_Bitmap.Width; x++)
+                for (int y = 0; y<Set_Bitmap.Height; y++){
+                    color = Set_Bitmap.GetPixel(x, y);
+                    
+                    bright = color.R < max ? 255 : 0;
+                    Set_Bitmap.SetPixel(x, y,Color.FromArgb(bright, bright, bright));
+                }
+
+}
+        
+
+    }
+}
---
+++ Perceptron/Perceptron/Perceptron_init.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Perceptron
+{
+    class Perceptron_init
+    {
+        private double a; //학습율 
+        private double y; //가중치값 
+        private double NET;
+        private int val;
+        private int Pattern;
+        private int[,] Input_Array; //출력값 (-1<0<1) 
+        private double[] Output_Array; //목표값 
+        private double[] firstW; //가중치값 
+        private double[,] w; //가중치.
+
+        public Perceptron_init(int[][] setVal) {
+            init();
+           int[] ss= new int [25]{
+               0,0,0, 0, 1,
+              0,0,0, 0, 1,
+              0,0,0, 0, 1,
+              0,0,0, 0, 1,
+              0,0,0, 0, 1 };
+            calc();
+            Console.WriteLine("학습 끝 2번째 시작");
+            //calc(ss);
+        }
+        private void init() {
+            w = new double[2, 3] { { 0.2, 0.1, -0.1 }, { 0, 0, 0 } };
+            Input_Array = new int[9, 25] {
+            { 0,0,0, 0, 1,
+              0,0,0, 0, 1,
+              0,0,0, 0, 1,
+              0,0,0, 0, 1,
+              0,0,0, 0, 1 },
+
+            { 1, 1, 1,1,1,
+              0,0,0, 0, 1,
+              1, 1, 1,1,1,
+              1, 0, 0,0,0,
+              1, 1, 1,1,1},
+
+            { 1,1,1, 1, 1,
+              0,0,0, 0, 1,
+              1,1,1, 1, 1,
+              0,0,0, 0, 1,
+              1,1,1, 1, 1},
+
+            { 1,0, 1, 0,0,
+              1,0, 1, 0,0,
+              1,1, 1, 1,1,
+              0,0, 1, 0,0,
+              0,0, 1, 0,0},
+
+            { 1,1, 1, 1,1,
+              1,0, 0, 0,0,
+              1,1, 1, 1,1,
+              0,0, 0, 0,1,
+              1,1, 1, 1,1},
+
+            { 1,0, 0, 0,0,
+              1,0, 0, 0,0,
+              1,1, 1, 1,1,
+              1,0, 0, 0,1,
+              1,1, 1, 1,1},
+
+            { 1,1, 1, 1,1,
+              1,0, 0, 0,1,
+              1,0, 0, 0,1,
+              0,0, 0, 0,1,
+              0,0, 0, 0,1},
+
+            { 1,1, 1, 1,1,
+              1,0, 0, 0,1,
+              1,1, 1, 1,1,
+              1,0, 0, 0,1,
+              1,1, 1, 1,1},
+
+             { 1,1, 1, 1,1,
+              1,0, 0, 0,1,
+              1,1, 1, 1,1,
+              0,0, 0, 0,1,
+              0,0, 0, 0,1},
+
+
+             }; 
+            Output_Array = new double[9] { -1, 1, 1, 1,1,1,1,1,1 };
+        }
+        private void NET_calc(double aNET){
+            double T = 0;
+            if (T < aNET)
+                y = 1;
+            else if (T == aNET)
+                y = 0;
+            else
+                y = -1;
+            Console.WriteLine("y :" + y);
+        }
+        private void calc() {
+            double[] arr = new double[9];
+            double count = 0;
+           
+            a = 1;
+            NET = 0;
+            val = arr.Length;
+            Pattern = Output_Array.Length;
+            do
+            {
+                count = 0;
+                for (int j = 0; j < Pattern; j++){
+                    for (int i = 0; i < val; i++){
+                        arr[i] = arr[i] + w[0, i] * Input_Array[j, i];
+                        NET = NET + arr[i];
+                        Console.WriteLine(arr[i]);
+                        arr[i] = 0;
+                    }
+                    Console.WriteLine("합계" + NET);
+                    NET_calc(NET);
+                    if (y == Output_Array[j]){
+                        count++;
+                        Console.WriteLine("목표값 일치");
+                        for (int i = 0; i < val; i++){
+                            w[1, i] = 0;
+                            w[1, i] = w[0, i] + w[1, i];
+                            Console.Write(w[1, i] + ", ");
+                        }
+                    }
+
+                    else
+                    {
+                        Console.WriteLine("가중치 조정");
+                        for (int i = 0; i < val; i++){
+                            w[0, i] = a * (Output_Array[j] - y) * Input_Array[j, i];
+                            w[0, i] = w[0, i] + w[1, i];
+                            Console.Write(w[0, i] + ", ");
+                        }
+                    }
+
+                    NET = 0;
+                    Console.WriteLine();
+                    Console.WriteLine("================================");
+                }
+            } while (count < Pattern);
+            // Console.WriteLine("Input"+ Input+ "Patten"+Patten);
+        }
+        private void calc(int[] setArray)
+        {
+            double[] arr = new double[3];
+            double count = 0;
+
+            a = 1;
+            NET = 0;
+            val = arr.Length;
+            Pattern = Output_Array.Length;
+            do
+            {
+                count = 0;
+                for (int j = 0; j < Pattern; j++) {
+                    for (int i = 0; i < val; i++) {
+                        arr[i] = arr[i] + w[0, i] * setArray[i];
+                        NET = NET + arr[i];
+                        arr[i] = 0;
+                    }
+                    NET_calc(NET);
+                  
+                        count++;
+                       
+                        for (int i = 0; i < val; i++)
+                        {
+                            w[1, i] = 0;
+                            w[1, i] = w[0, i] + w[1, i];
+                            Console.Write(w[1, i] + ", ");
+                        }
+                    
+                    
+                    NET = 0;
+                    Console.WriteLine();
+                    Console.WriteLine("================================");
+                }
+            } while (count < Pattern);
+            // Console.WriteLine("Input"+ Input+ "Patten"+Patten);
+        }
+
+    }
+}
